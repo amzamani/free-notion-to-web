cabf4f14f26cbb5bee6dbf12c8313c48cc0603e8
amzamani <abuholyangels@gmail.com>
2023-08-20 21:53:22 +0530

    updated configs and facicons
    Signed-off-by: amzamani <abuholyangels@gmail.com>

--- site.config.ts
+++ site.config.ts
@@ -2,19 +2,23 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '437a047a1dfb4932bf54c3772ed11849',
+  //  boiler plate page default
+  // rootNotionPageId: '437a047a1dfb4932bf54c3772ed11849',
+
+  // toffeeblog page
+  rootNotionPageId :'e9523f6a50ae4cb6b10131cba57a786b',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: '',
-  author: '',
+  name: 'ToffeeMoney Blog',
+  domain: 'https://blog.toffeemoney.com/',
+  author: 'Zamani Tech Ventures Pvt Ltd',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'ToffeeMoney Blog: Say hello to organised spending and savvy shopping',
 
   // social usernames (optional)
   twitter: '',
@@ -22,7 +26,7 @@ export default siteConfig({
   linkedin: '',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
-  // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
+  // youtube: '@toffeemoney', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
@@ -31,7 +35,7 @@ export default siteConfig({
   defaultPageCoverPosition: 0.5,
 
   // whether or not to enable support for LQIP preview images (optional)
-  isPreviewImageSupportEnabled: true,
+  isPreviewImageSupportEnabled: false,
 
   // whether or not redis is enabled for caching generated preview images (optional)
   // NOTE: if you enable redis, you need to set the `REDIS_HOST` and `REDIS_PASSWORD`
